d97574e5b67348e2531c2ec08a0f438f55964bf4
Alessio Longo <it.cryv@gmail.com>
2024-07-29 21:52:21 +0200

    [Feature] Add points update and fidelityView

--- Package.swift
+++ Package.swift
@@ -7,14 +7,11 @@ let package = Package(
     name: "EasySwiftCommunication",
     platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EasySwiftCommunication",
             targets: ["EasySwiftCommunication"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "EasySwiftCommunication"),
         .testTarget(
